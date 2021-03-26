2ad1cad41467f0fe094d41b83985d2e7a7ce2ced
jbgour <jeanbaptiste.gourlet@hotmail.fr>
2021-03-26 18:44:21 +0100

    added cvs reader criterions

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -4,13 +4,23 @@ from mesa.time import RandomActivation
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
 from communication.preferences.Preferences import Preferences
+import csv
 
+
+def csv_to_dict(csv_path):
+    reader = csv.reader(open(csv_path, mode='r', encoding='utf-8-sig'))
+    preferences = {}
+    for row in reader:
+        key = row[0]
+        preferences[key] = row[1]
+    return preferences
 
 
 class ArgumentAgent(CommunicatingAgent):
     """
     TestAgent which inherit from CommunicatingAgent.
     """
+
     def __init__(self, unique_id, model, name):
         super().__init__(unique_id, model, name)
         self.preference = None
@@ -38,6 +48,7 @@ class ArgumentAgent(CommunicatingAgent):
 class ArgumentModel(Model):
     """ ArgumentModel which inherit from Model.
     """
+
     def __init__(self):
         self.schedule = RandomActivation(self)
         self.__messages_service = MessageService(self.schedule)
@@ -60,21 +71,18 @@ class ArgumentModel(Model):
 if __name__ == "__main__":
     argument_model = ArgumentModel()
 
+    preferences_path = {
+        'Agent1': 'agent1.csv',
+        'Agent2': 'agent2.csv'
+    }
+
     # To be completed
-    Agent1 = ArgumentAgent(1, argument_model, "Alice")
-    Agent2 = ArgumentAgent(2, argument_model, "Bob")
-    preferences = {'PRODUCTION_COST':'VERY_BAD'}
-    Agent2.generate_preferences(preferences)
-    print('Agent 2 Preferences: {}'.format(Agent2.get_preference().get_criterion_name_list()))
-#
-# PRODUCTION_COST = 0
-#     CONSUMPTION = 1
-#     DURABILITY = 2
-#     ENVIRONMENT_IMPACT = 3
-#     NOISE = 4
-#
-# VERY_BAD = 0
-#     BAD = 1
-#     AVERAGE = 2
-#     GOOD = 3
-#     VERY_GOOD = 4
+    Agent1 = ArgumentAgent(1, argument_model, "Agent1")
+    Agent2 = ArgumentAgent(2, argument_model, "Agent2")
+
+    Agent1.generate_preferences(csv_to_dict(preferences_path['Agent1']))
+    Agent2.generate_preferences(csv_to_dict(preferences_path['Agent2']))
+
+    print('Agent 1 Criterions: {}'.format(Agent1.get_preference().get_criterion_name_list()))
+    print('Agent 2 Criterions: {}'.format(Agent2.get_preference().get_criterion_name_list()))
+
