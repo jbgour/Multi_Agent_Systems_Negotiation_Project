d32ba92235bf8c6db34682281ac63b5cfa3713f5
jbgour <jeanbaptiste.gourlet@hotmail.fr>
2021-03-26 16:19:33 +0100

    downloaded file pw

---
+++ pw_argumentation.py
@@ -0,0 +1,50 @@
+from mesa import Model
+from mesa.time import RandomActivation
+
+from communication.agent.CommunicatingAgent import CommunicatingAgent
+from communication.message.MessageService import MessageService
+
+
+class ArgumentAgent(CommunicatingAgent):
+    """ TestAgent which inherit from CommunicatingAgent.
+    """
+    def __init__(self, unique_id, model, name):
+        super().__init__(unique_id, model, name)
+        self.preference = None
+
+    def step(self):
+        super().step()
+
+    def get_preference(self):
+        return self.preference
+
+    def generate_preferences(self, preferences):
+        # see question 3
+        # To be completed
+
+class ArgumentModel(Model):
+    """ ArgumentModel which inherit from Model.
+    """
+    def __init__(self):
+        self.schedule = RandomActivation(self)
+        self.__messages_service = MessageService(self.schedule)
+
+        # To be completed
+        # list_items = [...]
+        #
+        # a = ArgumentAgent(id, "agent_name")
+        # a.generate_preferences(preferences)
+        # self.schedule.add(a)
+        # ...
+
+        self.running = True
+
+    def step(self):
+        self.__messages_service.dispatch_messages()
+        self.schedule.step()
+
+
+if __name__ == "__main__":
+    argument_model = ArgumentModel()
+
+    # To be completed
